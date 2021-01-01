6aa678200434fcd71ad5dcd821b3f894baf8c037
alexanderhess <Hess.alexander1@web.de>
2021-01-01 15:48:09 +0100

    slightly changes

--- Sorting_Arrays/sortNestedArrayElements.js
+++ Sorting_Arrays/sortNestedArrayElements.js
@@ -1,18 +1,18 @@
-let arr = [[1, 2 ,3, 5, 6 ], [5,4], [2 ,3 ,2]];
+let arrs = [[1, 2 ,3, 5, 6 ], [5,4], [2 ,3 ,2]];
 let biggestLength = 0;
-arr = arr.filter(o => o.length != 0)
+arrs = arrs.filter(o => o.length != 0)
 
-for (let i = 0; i < arr.length; i++){
-    if (arr[i].length > biggestLength){
-        biggestLength = arr[i].length;
+for (let i = 0; i < arrs.length; i++){
+    if (arrs[i].length > biggestLength){
+        biggestLength = arrs[i].length;
     } 
 }
 
-let result = [];
+const result = [];
 for (let l = 0; l < biggestLength; l++){
-    for (let k = 0; k < arr.length; k++){
-        if(arr[k][l] !== undefined){
-            result.push(arr[k][l])
+    for (let k = 0; k < arrs.length; k++){
+        if(arrs[k][l] !== undefined){
+            result.push(arrs[k][l])
         }else{
             break;
         }
